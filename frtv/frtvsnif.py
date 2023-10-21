32c3ca06b68b1566e8f975041a41505ec0b0583b
Zern <75311133+BG47510@users.noreply.github.com>
2023-10-21 13:26:43 +0200

    Update frtvsnif.py
    Mise à jour du code

--- frtv/frtvsnif.py
+++ frtv/frtvsnif.py
@@ -4,13 +4,27 @@
 import requests
 import re
 import sys
+import random
 
-# l’User-Agent et le Referer dans l’en-tête de la requête.
-# Pour imiter une session de navigation d’un utilisateur humain.
+# User-Agent laisse des informations dans les logs du site visité.
+# Elles permettent de savoir si leur site est crawlé.
+# Changer son user agent ua :
+ua = [
+    'Mozilla/5.0 (X11; Linux x86_64; rv:100.0) Gecko/20100101 Firefox/100.0',
+    'Mozilla/5.0 (Android 12; Mobile; rv:100.0) Gecko/100.0 Firefox/100.0',
+    'Mozilla/5.0 (X11; U; Linux i686; fr; rv:1.7.2) Gecko/20040804',
+    'Mozilla/5.0 (Windows NT 6.2; WOW64; Trident/7.0; rv:11.0) like Gecko',
+    'Mozilla/5.0 (Windows NT 10.0; rv:120.0) Gecko/20100101 Firefox/120.0',
+    'Dalvik/2.1.0 (Linux; U; Android 13; POCO F1 Build/TQ3A.230805.001)',
+    'Mozilla/5.0 (X11; Linux x86_64; rv:5.1) Goanna/20220721 PaleMoon/31.1.1',
+]
+
+# l’User-Agent et le Referer dans l’en-tête de la requête.
 headers = {
-    "User-Agent": "QwantMobile/2.0 (Android 5.1; Tablet; rv:61.0) Gecko/61.0 Firefox/59.0 QwantBrowser/61.0",
-    "Referer": "https://www.qwant.com/",
+    "User-Agent": random.choice(ua),
+    "Referer": "https://www.france.tv/",
 }
+
 
 erreur = 'https://raw.githubusercontent.com/BG47510/Zap/main/assets/error.m3u8'
 
@@ -22,6 +36,7 @@ def snif(line):
         print(erreur)
     else:
         print(lien)
+    sleep(randint(15,25))
 
 # L’objet Session conserve les paramètres entre plusieurs requêtes.
 # Il conserve également les cookies entre toutes les requêtes de la même instance Session.
@@ -44,4 +59,3 @@ with open('frtv/frtvsource.txt', encoding='utf-8') as f:
             print(f'#EXTINF:-1 tvg-id="{idepg}" tvg-name="{tvgname}" group-title="{grp}", {chnom}')
         else:
             snif(line)
-
