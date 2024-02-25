7a9a09d6fa30af6a145f26818cd3ecb1351fefae
Zern <75311133+BG47510@users.noreply.github.com>
2024-02-25 11:19:49 +0100

    Add files via upload

---
+++ py/frtv.py
@@ -0,0 +1,28 @@
+#! /usr/bin/python3
+# par github.com/BG47510
+import requests
+import sys
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:121.0) Gecko/20100101 Firefox/121.0",
+    "Referer": "http://www.callofliberty.fr/",
+    "visitorCountry":"FR"
+}
+
+erreur = requests.get("https://raw.githubusercontent.com/BG47510/Zap/main/assets/error.m3u8").text
+
+s = requests.session()
+
+def snif(url):
+    try:
+        source = s.get(url, headers=headers)
+        m3u8 = (source).text
+    except Exception as e:
+        m3u8 = erreur
+    finally:
+        print(m3u8)
+
+clic = snif(sys.argv[1])
+
+
+
