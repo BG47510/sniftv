dd4f230988e6ecf244c71c288d7f6613ed436cf0
Zern <75311133+BG47510@users.noreply.github.com>
2024-03-06 10:07:26 +0100

    Update bouy.py

--- py/bouy.py
+++ py/bouy.py
@@ -5,7 +5,7 @@ import requests
 import sys
 
 headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115.0",
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:121.0) Gecko/20100101 Firefox/121.0",
     "authority": "mediainfo.tf1.fr",
 }
 params = {
